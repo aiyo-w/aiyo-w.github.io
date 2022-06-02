3d7b2a84ffeb7590684c4e8243e6209871f81161
aiyo-w <aiyo_w@outlook.com>
2022-06-02 15:02:57 +0800

    update share更改为aiyo-ui、完成布局

---
+++ src/components/aiyo-ui.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import './aiyo-ui.css';
+
+export class AiyoUI extends React.Component {
+    render() {
+        return (
+            <div className="content aiyo-ui-container">
+                <div className="container">
+                    <div className="aiyo-ui-show"></div>
+                    <div className="introduce aiyo-ui-introduce">
+                        <span className="title">Aiyo UI</span>
+                        <span className="title">Use react and typescript.</span>
+                        <span>这里介绍一些玩法，这里介绍一些玩法，这里介绍一些玩法</span>
+                        <span>
+                            <a href="../../helloworld/index.html">Goto See</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
